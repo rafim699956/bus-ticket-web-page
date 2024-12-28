ad59776f0bf86df02c0581ef54f24dc7c24fd5f4
rafim699956 <rafim699956@gmail.com>
2024-12-28 17:17:39 +0600

    create some javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,10 @@
-document.getElementById("toggle-btn").addEventListener("click", function () {
-	const list = document.getElementById("list");
-	list.classList.toggle("hidden");
-});
+// document.getElementById("toggle-btn").addEventListener("click", function () {
+// 	const list = document.getElementById("list");
+// 	list.classList.toggle("hidden");
+// });
+// 30.1 end
+// 30.3 end
+// 30.4 end
+const add = (a, b) => a + b; // arrow function in one line
+console.log(add(5, 6));
+// 30.5 end
